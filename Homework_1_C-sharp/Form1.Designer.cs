f5614fd672964c43ced7d42b0d650e7fb1bf50a9
wodo96 <renzo.cuomo1996@gmail.com>
2022-10-03 20:35:55 +0200

    -Added text on top -Now the buttons, RichTextBox and label autosize themselves

--- Homework_1_C-sharp/Form1.Designer.cs
+++ Homework_1_C-sharp/Form1.Designer.cs
@@ -33,13 +33,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.labelResponseOutput = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(194, 47);
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.richTextBox1.Location = new System.Drawing.Point(184, 98);
+            this.richTextBox1.MaximumSize = new System.Drawing.Size(400, 300);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(402, 164);
+            this.richTextBox1.Size = new System.Drawing.Size(400, 147);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -57,9 +60,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(194, 217);
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(222, 265);
+            this.button1.MaximumSize = new System.Drawing.Size(300, 0);
+            this.button1.MinimumSize = new System.Drawing.Size(50, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 23);
+            this.button1.Size = new System.Drawing.Size(100, 25);
             this.button1.TabIndex = 2;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,28 +77,43 @@
             // 
             this.labelResponseOutput.AccessibleName = "labelResponseOutput";
             this.labelResponseOutput.AutoSize = true;
-            this.labelResponseOutput.Location = new System.Drawing.Point(366, 264);
+            this.labelResponseOutput.Location = new System.Drawing.Point(350, 300);
+            this.labelResponseOutput.MaximumSize = new System.Drawing.Size(200, 0);
             this.labelResponseOutput.Name = "labelResponseOutput";
-            this.labelResponseOutput.Size = new System.Drawing.Size(38, 15);
+            this.labelResponseOutput.Size = new System.Drawing.Size(42, 15);
             this.labelResponseOutput.TabIndex = 3;
-            this.labelResponseOutput.Text = "label1";
+            this.labelResponseOutput.Text = "Save_1";
             this.labelResponseOutput.Visible = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(453, 217);
+            this.button2.Location = new System.Drawing.Point(435, 265);
+            this.button2.MaximumSize = new System.Drawing.Size(100, 25);
+            this.button2.MinimumSize = new System.Drawing.Size(50, 25);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.Size = new System.Drawing.Size(100, 25);
             this.button2.TabIndex = 4;
             this.button2.Text = "Empty";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(326, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 45);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Write the text that you want to save.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelResponseOutput);
             this.Controls.Add(this.button1);
@@ -113,5 +134,6 @@
         private Button button1;
         private Label labelResponseOutput;
         private Button button2;
+        private TextBox textBox1;
     }
 }
